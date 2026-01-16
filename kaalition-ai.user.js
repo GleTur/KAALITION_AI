6844692995e753fe1aaa292432f869f48973d918
GleTur <glebturutin712@gmail.com>
2026-01-16 21:05:39 +0500

    Update kaalition-ai.user.js

--- kaalition-ai.user.js
+++ kaalition-ai.user.js
@@ -6,6 +6,10 @@
 // @author       News Official при поддержки #КААЛИЦИЯ
 // @supportURL   https://t.me/news_itd
 // @match        https://xn--d1ah4a.com/*
+// @updateURL    https://github.com/GleTur/KAALITION_AI/raw/refs/heads/main/kaalition-ai.user.js
+// @downloadURL  https://github.com/GleTur/KAALITION_AI/raw/refs/heads/main/kaalition-ai.user.js
+// @supportURL   https://github.com/GleTur/KAALITION_AI/issues
+// @homepageURL  https://github.com/GleTur/KAALITION_AI
 // @grant        GM_xmlhttpRequest
 // @grant        GM_getValue
 // @grant        GM_setValue
